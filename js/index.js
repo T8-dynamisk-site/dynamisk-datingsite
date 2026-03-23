a80fc5a56cfe97c8ca69b27d0ffd2c22f5158baa
julie <jula0007@stud.ek.dk>
2026-03-23 13:52:43 +0100

    index rettelser med kode vi har lært

--- js/index.js
+++ js/index.js
@@ -1,48 +1,36 @@
-const burgerBtn = document.getElementById("burgerBtn");
-const mobileMenu = document.getElementById("mobileMenu");
-
-burgerBtn.addEventListener("click", function () {
-  mobileMenu.classList.toggle("show");
-});
 const popularGrid = document.getElementById("popularGrid");
 
-async function loadPopularUsers() {
-  try {
-    const response = await fetch("https://dummyjson.com/users");
-    const data = await response.json();
+fetch("https://dummyjson.com/users")
+  .then((res) => res.json())
+  .then((data) => {
+    showUsers(data.users.slice(0, 8));
+  });
 
-    const users = data.users.slice(0, 8);
+function showUsers(userArray) {
+  popularGrid.innerHTML = "";
 
-    popularGrid.innerHTML = users
-      .map((user) => {
-        return `
-          <a href="profil.html?id=${user.id}" class="user-card">
-            <img src="${user.image}" alt="${user.firstName} ${user.lastName}" class="user-card-image">
+  userArray.forEach((user) => {
+    popularGrid.innerHTML += `
+      <a href="profil.html?id=${user.id}" class="user-card">
+        <img src="${user.image}" alt="${user.firstName} ${user.lastName}" class="user-card-image">
 
-            <div class="user-card-info">
-              <div class="user-card-top">
-                <h3>${user.firstName} ${user.lastName}</h3>
-                <span>${user.age}</span>
-              </div>
+        <div class="user-card-info">
+          <div class="user-card-top">
+            <h3>${user.firstName} ${user.lastName}</h3>
+            <span>${user.age}</span>
+          </div>
 
-              <p class="user-info-row">
-  <img src="img/gender.svg" alt="køn" class="icon">
-  ${user.gender === "female" ? "Kvinde" : "Mand"}
-</p>
+          <p class="user-info-row">
+            <img src="img/gender.svg" alt="køn" class="icon">
+            ${user.gender === "female" ? "Kvinde" : "Mand"}
+          </p>
 
-<p class="user-info-row">
-  <img src="img/location.svg" alt="by" class="icon">
-  ${user.address.city}
-</p>
-            </div>
-          </a>
-        `;
-      })
-      .join("");
-  } catch (error) {
-    popularGrid.innerHTML = `<p>Kunne ikke hente brugere.</p>`;
-    console.error("Fejl ved hentning af brugere:", error);
-  }
+          <p class="user-info-row">
+            <img src="img/location.svg" alt="by" class="icon">
+            ${user.address.city}
+          </p>
+        </div>
+      </a>
+    `;
+  });
 }
-
-loadPopularUsers();
