ec39e1c6af4b5c2f3c98915c34d388bb45f7db08
julie <jula0007@stud.ek.dk>
2026-03-23 14:01:09 +0100

    rettelser

--- js/index.js
+++ js/index.js
@@ -34,9 +34,3 @@ function showUsers(userArray) {
     `;
   });
 }
-const burgerBtn = document.getElementById("burgerBtn");
-const mobileMenu = document.getElementById("mobileMenu");
-
-burgerBtn.addEventListener("click", () => {
-  mobileMenu.classList.toggle("show");
-});
